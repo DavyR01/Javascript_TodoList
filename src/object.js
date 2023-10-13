d646b0b2c03aa6a346f38e5ad54b32c1bfaec163
Davy ROBERT <davyrobert90@gmail.com>
2023-10-13 12:14:08 +0200

    Chapitre 7 : Leçon 35 - Introduction aux objets

---
+++ src/object.js
@@ -0,0 +1,9 @@
+const a = {}
+const b = Object()
+const c = new Object;
+// const d = Object.create();
+// const e = Object.assign();
+// const f = Object.fromEntries();
+
+console.log(a, b, c);
+// console.log(d, e, f);
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: path.resolve(__dirname, "src/index.js"),
+  entry: path.resolve(__dirname, "src/object.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js"
