308f61fd35f7e86afb1640e405570909eb1f5ecd
Davy ROBERT <davyrobert90@gmail.com>
2023-10-13 13:21:38 +0200

    Chapitre 7 : Leçon 36 - Les propriétés des objets

---
+++ README.md
@@ -0,0 +1,3 @@
+Astuces : 
+
+   - Pour changer de fichier hotLoad, se rendre dans webpack.config.js et changer le nom du fichier correspondant : entry: path.resolve(__dirname, "src/object.js"),
--- src/object.js
+++ src/object.js
@@ -7,3 +7,31 @@ const c = new Object;
 
 console.log(a, b, c);
 // console.log(d, e, f);
+
+const terre = {
+      population: 7e7,
+      satellite: 'Lune',
+      temperature: {
+            min: -70,
+            max: 60,
+            current: {
+                  min: 10,
+                  max: 30
+            }
+      },
+      isOld: false,
+      getTemperature(){
+            return (this.temperature["min"])
+      },
+      getTemperature2: function() {
+            console.log(this.temperature["max"])
+      }
+}
+
+console.log("terre :", terre);
+console.log(terre.temperature); // without brackets with dot
+console.log(terre["temperature"]); // the same output than previous using brackets without dot
+
+console.log(terre["satellite"]); // Lune
+console.log(terre.getTemperature());
+terre.getTemperature2();
