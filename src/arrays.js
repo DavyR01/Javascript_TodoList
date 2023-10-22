7c2fcdb8602f294a0e911ddff208d65bfb0c30c5
Davy ROBERT <davyrobert90@gmail.com>
2023-10-22 20:20:47 +0200

    Chapitre 9 : Leçon 59 - Accéder aux propriétés d'un tableau

--- src/arrays.js
+++ src/arrays.js
@@ -1,4 +1,4 @@
-const br = () => console.log('\n');
+const br = () => console.log('\n'); br()
 
 // **************** 58) Introduction aux tableaux  **************************
 
@@ -18,19 +18,45 @@ console.log(arr5);
 console.log(typeof(arr5));
 console.log(Array.isArray(arr5)); // Permet de tester si la variable est un array ou non, retourne un boolean
 console.log(arr6);
+br()
 
 
+// **************** 59) Accéder aux propriétés d'un tableau  **************************
 
+let arr7 = ["soleil", "terre", 'lune'] 
 
+console.log((arr7));
+console.log(arr7[0]);
+console.log(arr7[arr7.length - 1]);
 
-// **************** 59)  **************************
+arr7.length = 0 // Reset un tableau sans modifier sa référence.
+console.log(arr7);
+
 // **************** 60)  **************************
+
+
 // **************** 62)  **************************
+
+
 // **************** 63)  **************************
+
+
 // **************** 64)  **************************
+
+
 // **************** 65)  **************************
+
+
 // **************** 66)  **************************
+
+
 // **************** 67)  **************************
+
+
 // **************** 68)  **************************
+
+
 // **************** 69)  **************************
+
+
 // **************** 70)  **************************
