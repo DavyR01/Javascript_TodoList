f1d7bc6401db4ea8268a34c765cffea9ec7c9a1d
Davy ROBERT <davyrobert90@gmail.com>
2023-10-24 21:50:43 +0200

    Chapitre 10 : Leçon 73 - Importer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-// import "./lib"
-import obj, { lib5 } from "./lib"
-import { foo, libNewName1 } from "./lib" // import nommé
 
+const br = () => console.log('\n');
 
-// **************** 72) Les modules : Exporter **************************
+// **************** 73) Les modules : Importer **************************
+
+import "./lib.js" //Permet d'exécuter le module lib mais on a besoin de récupérer aucun module.
+import * as all from "./lib.js" // Permet de tout exporter le module d'un seul coup 
+
+import exportDefault from './lib.js' // Pour les exports par défaut, on n'utilise pas d'accolades.
+import objDefault from "./lib.js" // Lorsque export par défault, pas d'import nommé.
+
+import { foo, libNewName1, lib2 as libNewName2, lib5 } from "./lib.js" // imports nommés en un seul objet
 
 const lib = 'Ma constante lib dans index.js'
+const lib2 = 'Ma constante lib2 dans index.js'
+
 console.log(lib);
-console.log(libNewName1);
+console.log(libNewName1);br()
+console.log(lib2);
+console.log(libNewName2); br()
 
 console.log(lib5);
 console.log(foo);
-console.log(obj);
+
+console.log(all.foo); 
+all.hello()
+
+exportDefault()
+// console.log(objDefault);
--- src/lib.js
+++ src/lib.js
@@ -3,7 +3,7 @@
 
 console.log('LIB');
 
-const obj = {name: "object"}
+const objDefault = {name: "object"}
 
 const lib = 'Ma constante lib dans lib.js'
 const lib2 = 'Ma constante lib2'
@@ -14,8 +14,13 @@ const h = {
    foo: 'bar'
 }
 
+const z = 'world !'
+export function hello() {
+   console.log('Hello', z);
+}
 
-export { lib as libNewName1, lib2 as libNewName2, lib3, lib4 } 
+
+export { lib as libNewName1, lib2, lib3, lib4, objDefault } 
 // Possibilité d'exporter plusieurs identifiants dans un seul objet. Possibilité de mettre un alias, de renommer les identifiants pour exporter un élément qui a le même nom par exemple dans l'autre fichier où l'on veut l'exporter. (lib dans index.js par exemple)
 
 export const lib5 = 'Ma constante lib5 importé depuis lib.js'
@@ -26,5 +31,5 @@ export const { foo } = h // export nommé
 
 // On ne peut exporter qu'un seul élement par défaut par fichier. Ici obj ou function : 
 
-// export default function i() { }
-export {obj as default}
+export default function exportDefault() {console.log('export par défaut')}
+// export {objDefault as default}
