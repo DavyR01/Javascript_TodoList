32e297b5fe57bf4e120604fdec552738a071efae
Davy ROBERT <davyrobert90@gmail.com>
2023-10-26 13:31:32 +0200

    Chapitre 11 : Leçon 84 - Positionner des nœuds

--- src/index83.js
+++ src/index83.js
@@ -1,4 +1,55 @@
-// import './style/style.css'
+import './style/style.css'
+
+const app = document.querySelector('#app');
+
+const section = document.createElement("section");
+
+
+const image = document.createElement("img");
+const imageSrc = document.createAttribute("src");
+imageSrc.value = 'https://miro.medium.com/v2/resize:fit:640/1*kCD_6R3UkMHkpS_YgFoU9w.jpeg'
+
+image.setAttributeNode(imageSrc)
+
+
+const paragraphe = document.createElement("p");
+const paragrapheClass = document.createAttribute("class");
+paragrapheClass.value = 'text-primary'
+const paragrapheText = document.createTextNode("Je suis un paragraphe");
+
+paragraphe.setAttributeNode(paragrapheClass)
+
+
+const a = document.createElement("a");
+const aHref = document.createAttribute("href");
+aHref.value = 'https://dyma.fr/developer/list/chapters/core/5dfbc811c7b96a25633bce2c/lesson/javascript/5e091eb35ab87663d8d2d211/11/9'
+const aText = document.createTextNode("Dyma cours 83: Créer des nœuds");
+
+a.setAttributeNode(aHref)
+
+
+const comment = document.createComment('je suis un com')
+
+app.appendChild(section);
+section.append(image, /* paragraphe */);
+
+section.insertAdjacentElement('beforeend',paragraphe)
+// section.insertAdjacentElement('afterbegin',paragraphe)
+// section.insertAdjacentElement('beforebegin',paragraphe)
+// section.insertAdjacentElement('afterend',paragraphe)
+
+paragraphe.append(paragrapheText, a);
+a.appendChild(aText);
+
+// paragraphe.insertBefore(comment, a)
+
+
+
+
+
+
+
+
 
 
 /* app.innerHTML = `
@@ -14,29 +65,3 @@
    </p>
 </section>
 `; */
-
-
-const app = document.querySelector('#app');
-
-const section = document.createElement("section");
-
-
-const image = document.createElement("img");
-const imageSrc = document.createAttribute("src");
-imageSrc.value = 'https://miro.medium.com/v2/resize:fit:640/1*kCD_6R3UkMHkpS_YgFoU9w.jpeg'
-
-
-const paragraphe = document.createElement("p");
-const paragrapheClass = document.createAttribute("class");
-paragrapheClass.value = 'text-primary'
-const paragrapheText = document.createTextNode("Je suis un paragraphe");
-
-
-const a = document.createElement("a");
-const aHref = document.createAttribute("href");
-aHref.value = 'https://dyma.fr/developer/list/chapters/core/5dfbc811c7b96a25633bce2c/lesson/javascript/5e091eb35ab87663d8d2d211/11/9'
-const aText = document.createTextNode("Dyma cours 83: Créer des nœuds");
-
-
-const comment = document.createComment('je suis un com')
-
