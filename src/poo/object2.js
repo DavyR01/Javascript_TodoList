35253c236a045895ec32227e48232967f7e3955c
Davy <100140271+DavyR01@users.noreply.github.com>
2023-11-08 11:39:05 +0100

    Chapitre 20: Leçon 146 - Les accesseurs et les mutateurs

--- src/poo/object2.js
+++ src/poo/object2.js
@@ -26,3 +26,52 @@ console.log(descriptor);
 for (let key in obj) {
    console.log(key);
 }
+
+
+
+// **************** 146) Les accesseurs et les mutateurs **************************
+
+const user = {
+   firstname: "roger",
+   lastname: 'rabbit',
+   get fullname1() {
+      return this.firstname + " " + this.lastname;
+   },
+   fullname2: function () {
+      return this.firstname + " " + this.lastname;
+   },
+   set fullname1(value) {
+      [this.firstname, this.lastname] = value.split(" ")
+   }
+}
+
+console.log(user.fullname1);
+console.log(user.fullname2());
+
+user.fullname1 = 'Freedy Mercury';
+console.log(user.fullname1);
+
+
+
+// Other way : 
+
+const user2 = {
+   firstname: "Robert",
+   lastname: 'DENIRO',
+}
+
+Object.defineProperty(user2, "fullname1", {
+   get: function() {
+      return this.firstname + " " + this.lastname;
+   },
+   set() {
+      [this.firstname, this.lastname] = value.split(" ")
+   },
+   enumerable: true,
+   configurable: true
+})
+
+const descriptor2 = Object.getOwnPropertyDescriptor(user2, 'fullname1')
+
+console.log(descriptor2);
+console.log(user2.fullname1);
