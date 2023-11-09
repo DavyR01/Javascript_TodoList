3a5eb65aa8c70b1421e9cccc192f72ee7f18d9bf
Davy <100140271+DavyR01@users.noreply.github.com>
2023-11-09 22:40:56 +0100

    Chapitre 21: Leçon 160 - Introduction à la gestion d'erreur

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    mainEntry : path.resolve(__dirname, "src/class/class.js"), // Important
+    mainEntry : path.resolve(__dirname, "src/errors/errors.js"), // Important
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -26,7 +26,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/index.html") // Important
+      template: path.resolve(__dirname, "src/errors/errors.html") // Important
     })
   ],
   devtool: "source-map",
