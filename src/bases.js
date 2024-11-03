c8a1e1860c243cc076d44b99c49c5ba7b3ef3cfa
Davy <100140271+DavyR01@users.noreply.github.com>
2024-11-03 18:33:26 +0100

    Chapitre 3: Leçon 21 - Opérateur de coalescence des nuls + launch configuration javascript debugger launch.json

--- src/bases.js
+++ src/bases.js
@@ -6,30 +6,41 @@ console.log(test);
 
 // console.log($);
 
-// Ouvrez la console Chrome en faisant CTRL + MAJ + I
-// ou clic droit dans la fenêtre blanche puis inspecter puis onglet console.
+//? Ouvrez la console Chrome en faisant CTRL + MAJ + I ou clic droit dans la fenêtre blanche puis inspecter puis onglet console.
 console.info("test");
 console.warn("test");
 console.error("test");
 console.log("test");
 
-// On peut afficher le temps passé entre l'exécution de la ligne suivante
-// et de l'exécution de la ligne d'après :
+//? On peut afficher le temps passé entre l'exécution de la ligne suivante et de l'exécution de la ligne d'après :
 console.time();
 console.timeEnd();
 
-// Affichage d'un tableau, nous verrons en détails les tableaux dans les
-// leçons suivantes :
+//? Affichage d'un tableau, nous verrons en détails les tableaux dans les leçons suivantes :
 console.table(["chat", "chien", "koala"]);
 console.table({ animal: "lama", animal2: "dromadaire", animal3: "chameau" });
 
 
-// Affichera une erreur pour le deuxième car l'assertion est fausse :
+//? Affichera une erreur pour le deuxième car l'assertion est fausse :
 console.assert(true, "Bonjour 1 !");
 console.assert(false, "Bonjour 2 !");
 
-// Compter le nombre d'exécution :
-// Nous étudierons les boucles en détails plus tard également.
+//? Compter le nombre d'exécution : Nous étudierons les boucles en détails plus tard également.
 for (let i = 0; i < 5; i++) {
 	console.count();
 }
+
+// **************** 21) Opérateur de coalescence des nuls ****************
+
+
+const user = {}
+let username = user.username || "John Doe"
+let username3 = 1 || "John Doe"
+let username2 = "" || "John Doe"
+console.log(username, username2, username3);
+
+let username4 = 0 ?? "Jane"
+let username5 = 1 ?? "Jane"
+let username6 = undefined ?? "Jane"
+console.log(username4, username5, username6);
+
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    mainEntry : path.resolve(__dirname, "src/todo/index92.js"), // Important
+    mainEntry : path.resolve(__dirname, "src/bases.js"), // Important
   },
   output: {
     path: path.resolve(__dirname, "dist"),
@@ -26,7 +26,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/todo/index92.html") // Important
+      template: path.resolve(__dirname, "src/index.html") // Important
     })
   ],
   devtool: "source-map",
