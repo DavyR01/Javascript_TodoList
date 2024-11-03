7180307353df7b51146f9db7e94d5a45b45d30d3
Davy <100140271+DavyR01@users.noreply.github.com>
2024-11-03 19:57:01 +0100

    Chapitre 17: Leçon 132 - Les formats des Dates en chaîne de caractères

--- src/dates.js
+++ src/dates.js
@@ -1,3 +1,5 @@
+const br = () => console.log("");
+
 // **************** 131) Déclarer une date en JavaScript ****************
 
 //? Date est un objet global JavaScript essentiel car il permet de manipuler les dates. 
@@ -9,7 +11,29 @@ console.log("Today is ... ", today);
 console.log("Today is ... ", today.toString());
 
 const date1 = new Date(0); //? Timestamp = durée en millisecondes qui s'est écoulée depuis la date d'origine de création 1970
-console.log(date1);
+console.log(date1); // Thu Jan 01 1970 01:00:00 GMT+0100 (heure normale d’Europe centrale)
 
-const date2= new Date(2020, 0, 7, 10, 50)
+const date2 = new Date(2020, 0, 7, 10, 50)
 console.log(date2);
+
+
+// **************** 132) Les formats des Dates en chaîne de caractères ****************
+
+//? Le z à la fin du format ISO JavaScript, signifie que le fuseau horaire est celui de Greenwich
+br()
+const today2 = new Date();
+const today3 = new Date().toString();
+// const today3 = new Date("2024-11-03T18:54:16.747Z");
+
+console.log(typeof(today2));
+console.log(typeof(today3));
+
+
+console.log(today2); // Sun Nov 03 2024 19:54:16 GMT+0100 (heure normale d’Europe centrale)
+console.log(today2.toISOString()); // 2024-11-03T18:54:16.747Z //? On convertit le date en string format ISO pour pouvoir le stocker dans d'autres environnements comme les bases de données afin de le standardiser au fuseau horaire de Greenwich. Utile pour la manipulation.
+console.log(today2.toUTCString()); // Sun, 03 Nov 2024 18:54:16 GMT
+console.log(today2.toDateString()); // Sun Nov 03 2024
+
+
+// **************** 133) Manipuler des timestamps ****************
+
