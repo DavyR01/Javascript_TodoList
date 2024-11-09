0f8a59e4e92f954dcefa479b989ee271822f745a
Davy <100140271+DavyR01@users.noreply.github.com>
2024-11-09 14:52:39 +0100

    Chapitre 27:  Le drag and drop

--- src/chapter27_drag&drop/avancee.js
+++ src/chapter27_drag&drop/avancee.js
@@ -1,5 +1,5 @@
 
-import './style.css';
+import './avancee.css';
 
 // **************** 204) Drag and Drop avancé sans l'API ******************
 
@@ -11,37 +11,42 @@ let container;
 picsou.addEventListener('dragstart', (event) => event.preventDefault());
 
 const move = (event) => {
-  picsou.style.left = event.pageX - shiftX + 'px';
-  picsou.style.top = event.pageY - shiftY + 'px';
+   picsou.style.left = event.pageX - shiftX + 'px';
+   picsou.style.top = event.pageY - shiftY + 'px';
 };
 
 const mouseMove = (event) => {
-  picsou.hidden = true;
-  const elem = document.elementFromPoint(event.pageX, event.pageY);
-  picsou.hidden = false;
-  if (elem && elem != container) {
-    container = elem;
-  }
-  move(event);
+   picsou.hidden = true;
+   const elem = document.elementFromPoint(event.pageX, event.pageY);
+   picsou.hidden = false;
+   if (elem && elem != container) {
+      container = elem;
+   }
+   move(event);
 };
 
 picsou.addEventListener('mousedown', (event) => {
-  picsou.classList.add('border');
-  picsou.style.position = 'absolute';
-  const rect = picsou.getBoundingClientRect();
-  shiftX = event.pageX - rect.left;
-  shiftY = event.pageY - rect.top;
-  move(event);
-  picsou.addEventListener('mousemove', mouseMove);
+   picsou.classList.add('border');
+   picsou.style.position = 'absolute';
+   const rect = picsou.getBoundingClientRect();
+   shiftX = event.pageX - rect.left;
+   shiftY = event.pageY - rect.top;
+   move(event);
+   picsou.addEventListener('mousemove', mouseMove);
 });
 
 picsou.addEventListener('mouseup', (event) => {
-  picsou.style.position = 'static';
-  picsou.style.left = '';
-  picsou.style.top = '';
-  picsou.classList.remove('border');
-  picsou.removeEventListener('mousemove', mouseMove);
-  if (container) {
-    container.appendChild(picsou);
-  }
+   picsou.style.position = 'static';
+   picsou.style.left = '';
+   picsou.style.top = '';
+   picsou.classList.remove('border');
+   picsou.removeEventListener('mousemove', mouseMove);
+   if (container) {
+      container.appendChild(picsou);
+   }
 });
+
+
+// **************** 204) Permuter des éléments sans l'API ******************
+
+
